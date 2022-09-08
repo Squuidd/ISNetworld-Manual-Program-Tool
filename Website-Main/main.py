342459d06fe8d50fb5117854b4409c741ab92e74
Squuidd <reesecooper1010@gmail.com>
2022-09-07 22:23:05 -0500

    Basic flask template added

---
+++ Website-Main/main.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template, request
+from wtforms.widgets.core import TextArea
+from flask_wtf import FlaskForm
+from wtforms import StringField, form
+
+app = Flask(__name__, template_folder="templates")
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+if __name__ == "__main__":
+    app.run()
+           
