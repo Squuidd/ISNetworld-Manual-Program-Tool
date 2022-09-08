1bf6c34ef01cd81a2dc4917392d3158e7356f01a
Squuidd <reesecooper1010@gmail.com>
2022-09-08 13:40:56 -0500

    PDF conversion added

--- Website-Main/safety_program_creator.py
+++ Website-Main/safety_program_creator.py
@@ -5,22 +5,24 @@ from xml.dom.minidom import Document
 from docx.enum.style import WD_STYLE_TYPE
 from docx.enum.table import WD_ALIGN_VERTICAL
 import docx
+from docx2pdf import convert
 
 COMPANY_NAME = input("Company name: \n")
 chosen_file = input("Choose file: \n")
 # chosen_file = "abrasive blasting.docx"
 
-script_dir = os.path.dirname(__file__) # absolute dir the script is in
-rel_path = f"Safety Programs/{chosen_file}"
-abs_file_path = os.path.join(script_dir, rel_path)
+def findPath(file_name):
+    script_dir = os.path.dirname(__file__) # absolute dir the script is in
+    rel_path = f"Safety Programs/{file_name}"
+    abs_file_path = os.path.join(script_dir, rel_path)
+    return abs_file_path
 
-def getText(filename): # use abs_file_path for filenames
-    doc = docx.Document(filename)
+def getText(path): # use abs_file_path for filenames
+    doc = docx.Document(path)
     fullText = []
     for para in doc.paragraphs:
         fullText.append(para.text)
     return '\n'.join(fullText)
-
 
 def parseTable(table):
     data = []
@@ -43,8 +45,7 @@ def parseTable(table):
 
         return data
 
-
-def companyStyle():
+def companyStyle(document):
     styles = document.styles
     style = styles.add_style("Company", WD_STYLE_TYPE.PARAGRAPH)
     
@@ -53,23 +54,33 @@ def companyStyle():
     font.bold = True
     font.size = docx.shared.Pt(18)
 
-def addCompanyName(table):
-    companyStyle()
+def addCompanyName(table, doc):
+    companyStyle(doc)
 
     name_row = table.rows[0].cells
     company_name = name_row[0]
     company_name.text = COMPANY_NAME
     
     paragraph = company_name.paragraphs[0]
-    paragraph.style = document.styles["Company"]
+    paragraph.style = doc.styles["Company"]
     company_name.vertical_alignment = WD_ALIGN_VERTICAL.CENTER
-    document.save(f"Output/{chosen_file}")
 
-document = docx.Document(abs_file_path)
-section = document.sections[0]
-header = section.header
+    save_path = f"Output/{chosen_file}"
+    doc.save(save_path)
+    createPDF(save_path, save_path)
+
+def createPDF(start_path, save_path):
+    size = len(start_path)
+    pdf_name = f"{start_path[:size - 5]}.pdf"
+    convert(start_path, pdf_name)
 
 
-for table in header.tables:
-    addCompanyName(table)
+def createSafetyProgram(path):
+    document = docx.Document(path)
+    section = document.sections[0]
+    header = section.header
 
+    for table in header.tables:
+        addCompanyName(table, document)
+
+createSafetyProgram(findPath(chosen_file))
