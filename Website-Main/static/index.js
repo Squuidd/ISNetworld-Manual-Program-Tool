c9f6d45f036edab1b85c6bc713b36b46e6fa625f
Squuidd <reesecooper1010@gmail.com>
2022-10-07 22:35:05 -0500

    selection list functional
    needs work with handling old line breaks

--- Website-Main/static/index.js
+++ Website-Main/static/index.js
@@ -13,40 +13,68 @@ async function parsePromise(promise)
 {
     let value = await promise;
 
-    let program_names = [];
+    let program_data = [];
     for(let i = 0; i < value.length; i++)
-    {
-        program_names.push(value[i][0]);
+    {   
+        data = [];
+        data.push(value[i][0]);
+        data.push(value[i][1]);
+
+        program_data.push(data);
     }
     
-    return program_names;
+    return program_data;
 }
 
 async function populateUnselected()
 {
-    let names = await parsePromise(getSP());
+    let program_data = await parsePromise(getSP());
 
-    for(let i = 0; i < names.length; i++)
+    unselected_parent = document.getElementById("unselected_parent");
+
+    for(let i = 0; i < program_data.length; i++)
     {   
         //TODO refactor this
         var node = document.createElement("button");
         node.classList.add("program_btn");
+        node.id = "program_" + program_data[i][1];
         node.onclick = moveList;
 
-        var textnode = document.createTextNode(names[i]);
+        var textnode = document.createTextNode(program_data[i][0]);
 
         node.appendChild(textnode);
         var lineBreak = document.createElement("br");
+        lineBreak.id = node.id + "_br";
 
-        document.getElementById("unselected_parent").appendChild(node);
-        document.getElementById("unselected_parent").appendChild(lineBreak);
+        unselected_parent.appendChild(node);
+        unselected_parent.appendChild(lineBreak);
     }
+
+    var lineBreak = document.createElement("br");
+    unselected_parent.prepend(lineBreak);
 }
 
 async function moveList()
 {
-    alert(this);
+    parent_id = this.parentNode.id;
 
+    program_btn = document.getElementById(this.id);
+    var lineBreak = document.createElement("br");
+
+    if(parent_id == "selected_parent")
+    {
+        parent = document.getElementById("unselected_parent");
+    }
+    else if(parent_id == "unselected_parent")
+    {
+        parent = document.getElementById("selected_parent");
+    }
+    
+    parent.prepend(program_btn);
+    parent.prepend(lineBreak);
+
+    old_br = document.getElementById(this.id + "_br");
+    old_br.remove();
 }
 
 populateUnselected();
